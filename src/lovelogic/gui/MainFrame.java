29f7fe56c8440a2492219742e0409a1d68c0c2b7
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2013-08-14 02:41:23 +0900

    add entry point for GUI test

--- src/lovelogic/gui/MainFrame.java
+++ src/lovelogic/gui/MainFrame.java
@@ -3,6 +3,9 @@ package lovelogic.gui;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame
@@ -17,5 +20,29 @@ public class MainFrame extends JFrame
 		add(pfPanel, BorderLayout.CENTER);
 
 		pack();
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("GUI test.");
+
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Exception e)
+		{
+		}
+
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				MainFrame f = new MainFrame();
+				f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+				f.setLocationRelativeTo(null);
+				f.setVisible(true);
+			}
+		});
 	}
 }
