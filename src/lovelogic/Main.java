809f205fab60a736453ea1e9332fe57ff6538f50
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2013-08-16 00:20:02 +0900

    delete old prove() method

--- src/lovelogic/Main.java
+++ src/lovelogic/Main.java
@@ -1,12 +1,7 @@
 package lovelogic;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import lovelogic.prover.Prover;
-import lovelogic.sequent.Deducer;
 import lovelogic.sequent.Sequent;
-import lovelogic.sequent.SequentList;
 import lovelogic.syntax.Formula;
 import lovelogic.syntax.parser.exception.ParserException;
 import util.MTree;
@@ -35,41 +30,6 @@ public class Main
 		{
 			e.printStackTrace();
 		}
-	}
-
-	public static boolean prove(Sequent s, int depth)
-	{
-		String indent = StringUtils.repeat(depth, "  ");
-
-		if (s.isAxiom())
-		{
-			System.out.println(indent + s + " (Axiom)");
-			return true;
-		}
-
-		List<SequentList> deductions = new ArrayList<SequentList>();
-		for (Formula a : s.getAllFormulae())
-		{
-			Deducer.getDeductionList(s, a, deductions);
-		}
-		for (SequentList subGoals : deductions)
-		{
-			boolean allProved = true;
-			for (Sequent subGoal : subGoals.getSubGoals())
-			{
-				if (!prove(subGoal, depth + 1))
-				{
-					allProved = false;
-					break;
-				}
-			}
-			if (allProved)
-			{
-				System.out.println(indent + s);
-				return true;
-			}
-		}
-		return false;
 	}
 
 	private static void showProof(MTree<Sequent> tree, int depth)
