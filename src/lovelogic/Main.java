260cba97ee348cb6c3d0d9313bbbb761b2476167
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2013-08-15 19:49:43 +0900

    externalize proving algorithm to class Prover

--- src/lovelogic/Main.java
+++ src/lovelogic/Main.java
@@ -3,6 +3,7 @@ package lovelogic;
 import java.util.ArrayList;
 import java.util.List;
 
+import lovelogic.prover.Prover;
 import lovelogic.sequent.Deducer;
 import lovelogic.sequent.Sequent;
 import lovelogic.sequent.SequentList;
@@ -19,7 +20,7 @@ public class Main
 		{
 			Formula x = Formula.parse("~((A \\/ B) /\\ (A \\/ ~B) /\\ (~A \\/ B) /\\ (~A \\/ ~B))");
 			Sequent s = Sequent.createGoal(x);
-			MTree<Sequent> proof = proveMin(s);
+			MTree<Sequent> proof = Prover.findMinimumProof(s);
 			if (proof != null)
 			{
 				System.out.println("Proved.");
@@ -69,47 +70,6 @@ public class Main
 			}
 		}
 		return false;
-	}
-
-	private static MTree<Sequent> proveMin(Sequent goal)
-	{
-		if (goal.isAxiom())
-		{
-			return MTree.of(goal);
-		}
-
-		MTree<Sequent> prMin = null;
-		List<SequentList> deductions = new ArrayList<SequentList>();
-		for (Formula a : goal.getAllFormulae())
-		{
-			Deducer.getDeductionList(goal, a, deductions);
-		}
-
-		for (SequentList subGoals : deductions)
-		{
-			MTree<Sequent> pr = MTree.of(goal);
-			for (Sequent subGoal : subGoals.getSubGoals())
-			{
-				MTree<Sequent> subpr = proveMin(subGoal);
-				if (subpr != null) 
-				{
-					pr.addSub(subpr);
-				}
-				else
-				{
-					pr = null;
-					break;
-				}
-			}
-			if (pr != null)
-			{
-				if (prMin == null || pr.getMaxDepth() < prMin.getMaxDepth())
-				{
-					prMin = pr;
-				}
-			}
-		}
-		return prMin;
 	}
 
 	private static void showProof(MTree<Sequent> tree, int depth)
---
+++ src/lovelogic/prover/Prover.java
@@ -0,0 +1,70 @@
+package lovelogic.prover;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lovelogic.sequent.Deducer;
+import lovelogic.sequent.Sequent;
+import lovelogic.sequent.SequentList;
+import lovelogic.syntax.Formula;
+import util.MTree;
+
+public class Prover
+{
+	private Prover() { }
+
+	public static MTree<Sequent> findProof(Sequent goal)
+	{
+		return prove(goal, false);
+	}
+
+	public static MTree<Sequent> findMinimumProof(Sequent goal)
+	{
+		return prove(goal, true);
+	}
+
+	private static MTree<Sequent> prove(Sequent goal, boolean minimize)
+	{
+		if (goal.isAxiom())
+		{
+			return MTree.of(goal);
+		}
+
+		MTree<Sequent> prMin = null;
+		List<SequentList> deductions = new ArrayList<SequentList>();
+		for (Formula a : goal.getAllFormulae())
+		{
+			Deducer.getDeductionList(goal, a, deductions);
+		}
+
+		for (SequentList subGoals : deductions)
+		{
+			MTree<Sequent> pr = MTree.of(goal);
+			for (Sequent subGoal : subGoals.getSubGoals())
+			{
+				MTree<Sequent> subpr = prove(subGoal, minimize);
+				if (subpr != null) 
+				{
+					pr.addSub(subpr);
+				}
+				else
+				{
+					pr = null;
+					break;
+				}
+			}
+			if (pr != null)
+			{
+				if (prMin == null || pr.getMaxDepth() < prMin.getMaxDepth())
+				{
+					prMin = pr;
+					if (!minimize)
+					{
+						break;
+					}
+				}
+			}
+		}
+		return prMin;
+	}
+}
