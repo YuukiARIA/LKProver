504700165fb6af7696fc52847c0e174b0ee0edcf
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2013-08-16 01:22:48 +0900

    add components for input

--- src/lovelogic/gui/MainFrame.java
+++ src/lovelogic/gui/MainFrame.java
@@ -1,16 +1,33 @@
 package lovelogic.gui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.WindowConstants;
+
+import lovelogic.gui.figure.ProofFigure;
+import lovelogic.gui.figure.ProofFigureBuilder;
+import lovelogic.prover.ProofStep;
+import lovelogic.prover.Prover;
+import lovelogic.sequent.Sequent;
+import lovelogic.syntax.Formula;
+import lovelogic.syntax.parser.exception.ParserException;
+import util.MTree;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame
 {
 	private ProofFigurePanel pfPanel;
+	private JTextField textInput;
+	private JButton buttonProve;
 
 	public MainFrame()
 	{
@@ -19,7 +36,48 @@ public class MainFrame extends JFrame
 		pfPanel = new ProofFigurePanel();
 		add(pfPanel, BorderLayout.CENTER);
 
+		textInput = new JTextField();
+		buttonProve = new JButton("Prove");
+		buttonProve.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				String input = textInput.getText();
+				prove(input);
+			}
+		});
+
+		JPanel panelBottom = new JPanel(new BorderLayout());
+		panelBottom.add(textInput, BorderLayout.CENTER);
+		panelBottom.add(buttonProve, BorderLayout.EAST);
+		add(panelBottom, BorderLayout.SOUTH);
+
 		pack();
+	}
+
+	private void prove(String input)
+	{
+		try
+		{
+			Formula x = Formula.parse(input);
+			Sequent s = Sequent.createGoal(x);
+			MTree<ProofStep> proof = Prover.findMinimumProof(s);
+			if (proof != null)
+			{
+				ProofFigure pf = ProofFigureBuilder.build(proof);
+				pfPanel.setProofFigure(pf);
+			}
+			else
+			{
+				JOptionPane.showMessageDialog(this, "Unprovable", "Result", JOptionPane.INFORMATION_MESSAGE);
+				pfPanel.setProofFigure(null);
+			}
+		}
+		catch (ParserException e)
+		{
+			JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args)
