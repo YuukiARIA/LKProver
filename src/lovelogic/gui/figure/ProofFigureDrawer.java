0620b2c977a60f8a2b6853d42e4a9e10768b764c
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2013-08-18 19:57:46 +0900

    create drawer for proof figures

---
+++ src/lovelogic/gui/figure/ProofFigureDrawer.java
@@ -0,0 +1,66 @@
+package lovelogic.gui.figure;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+
+public class ProofFigureDrawer
+{
+	private boolean needLayout;
+	private Font font;
+	private ProofFigure pf;
+
+	public void setProofFigure(ProofFigure pf)
+	{
+		this.pf = pf;
+		needLayout = true;
+	}
+
+	public void setFont(Font font)
+	{
+		this.font = font;
+		needLayout = true;
+	}
+
+	public Dimension getSize()
+	{
+		if (pf != null)
+		{
+			return new Dimension(pf.getWholeWidth(), pf.getWholeHeight());
+		}
+		return new Dimension(0, 0);
+	}
+
+	public void drawCenter(Graphics g, int x, int y, int width, int height)
+	{
+		if (pf == null)
+		{
+			return;
+		}
+
+		layout(g);
+
+		Font old = g.getFont();
+		if (font != null)
+		{
+			g.setFont(font);
+		}
+		pf.drawCenter(g, x, y, width, height);
+		g.setFont(old);
+	}
+
+	public void layout(Graphics g)
+	{
+		if (pf != null && needLayout)
+		{
+			Font old = g.getFont();
+			if (font != null)
+			{
+				g.setFont(font);
+			}
+			pf.layout(g);
+			g.setFont(old);
+			needLayout = false;
+		}
+	}
+}
